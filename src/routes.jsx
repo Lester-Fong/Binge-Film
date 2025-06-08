9f2c00af4e03d4e32dae98811640fea814949e23
Lester Fong <lesterfonggi123@gmail.com>
2025-06-08 18:41:40 +0800

    Add route for MovieDetails component

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Favorites from './pages/Favorites.jsx'
+import MovieDetails from './pages/MovieDetails.jsx'
 
 function AppRoutes() {
     return (
         <Routes>
-            <Route path="/" element={<Home/>}></Route>
-            <Route path="/favorites" element={<Favorites/>}></Route>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/favorites" element={<Favorites />}></Route>
+            <Route path="/movie/:id" element={<MovieDetails />}></Route>
         </Routes>
     )
 }
