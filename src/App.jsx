1e8acaca3b2d8be5300ef485235bc2a3a64b868d
Lester Fong <lesterfonggi123@gmail.com>
2025-06-09 20:49:13 +0800

    Fix formatting inconsistencies in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import './css/App.css'
-import AppRoutes from './routes'
-import Navbar from './components/Navbar'
-import { MovieProvider } from './contexts/MovieContext'
+import "./css/App.css";
+import AppRoutes from "./routes";
+import Navbar from "./components/Navbar";
+import { MovieProvider } from "./contexts/MovieContext";
 
 function App() {
   return (
     <>
       <MovieProvider>
         <Navbar />
-        <main className='main-content'>
+        <main className="main-content">
           <AppRoutes />
         </main>
       </MovieProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
