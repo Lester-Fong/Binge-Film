8cea39c3c031d6878375cd637f2a6ae2039cc298
Lester Fong <lesterfonggi123@gmail.com>
2025-06-25 19:38:18 +0800

    Enhance MovieCard component layout and information display

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,6 +1,8 @@
 
 import '../css/MovieCard.css'
 import { useMovieContext } from '../contexts/MovieContext';
+import { truncateText } from '../services/helper';
+import { Badge } from "@/components/ui/badge"
 
 
 export default function MovieCard({ movie }) {
@@ -18,7 +20,7 @@ export default function MovieCard({ movie }) {
     }
 
     return <>
-        <div className="movie-card">
+        <div className="movie-card w-100">
             <div className="movie-poster">
                 <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
                 <div className="movie-overlay">
@@ -26,9 +28,10 @@ export default function MovieCard({ movie }) {
                 </div>
             </div>
             <div className="movie-info">
-                <h3>{movie.title}</h3>
-                <p>{movie.release_date.split('-')[0]}</p>
+                <h3>{truncateText(movie.title, 15)}</h3>
+                <Badge variant="destructive" className="px-1">{movie.vote_average.toFixed(1)}</Badge>
             </div>
+            <p className='movie-date mb-1'>{movie.release_date.split('-')[0]}</p>
         </div>
     </>
 }
