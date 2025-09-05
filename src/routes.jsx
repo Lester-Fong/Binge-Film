a03a9a002c4d93992b9809ca07899f66ad2043ed
fedora lester <lesterfonggi123@gmail.com>
2025-09-05 21:38:09 +0800

    feat: update routing for TV shows with dedicated path and component

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Favorites from './pages/Favorites.jsx'
+import TVShows from './pages/TVShows.jsx'
 import MovieDetails from './pages/MovieDetails.jsx'
 import TVShowDetails from './pages/TVShowDetails.jsx'
 
@@ -8,9 +9,10 @@ function AppRoutes() {
     return (
         <Routes>
             <Route path="/" element={<Home />}></Route>
+            <Route path="/tvshows" element={<TVShows />}></Route>
             <Route path="/favorites" element={<Favorites />}></Route>
             <Route path="/movie/:id" element={<MovieDetails />}></Route>
-            <Route path="/tv/:id" element={<TVShowDetails />}></Route>
+            <Route path="/tvshow/:id" element={<TVShowDetails />}></Route>
         </Routes>
     )
 }
